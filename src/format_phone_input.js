893d0b000d1d4c2cdf373d061471dfbca725fc19
Pat Nakajima <patnakajima@gmail.com>
2010-09-09 11:40:08 -0400

    make a repo

---
+++ src/format_phone_input.js
@@ -0,0 +1,37 @@
+// format-phone-input
+(function($) {
+  function format(e) {
+    var input = $(this)
+    var digits = input.val().replace(/[^\d]/g, '');
+
+    console.info(e);
+
+    if (e.which == 8) {
+      return;
+    }
+    
+    if (digits.length < 3) {
+      return;
+    }
+
+    if ((digits.length >= 3 && digits.length <= 6)) {
+      var prefix = digits.substring(0, 3);
+      var rest = digits.substring(3, digits.length);
+      return input.val(prefix + '-' + rest);
+    }
+
+    if (digits.length > 6 && digits.length <= 10) {
+      var areaCode = digits.substring(0, 3);
+      var prefix = digits.substring(3, 6);
+      var rest = digits.substring(6, digits.length)
+      return input.val('(' + areaCode + ') ' + prefix + '-' + rest);
+    }
+    
+    return false;
+  }
+
+  $.fn.formatsPhone = function() {
+    this.bind('keypress', format);
+    this.attr('maxlength', '14');
+  };
+})(jQuery);
